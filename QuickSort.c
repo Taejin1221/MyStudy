1bd04a99c69cf6ecc5f2805981eb011a56cb8c3e
TAEJIN1221 <48683997+TAEJIN1221@users.noreply.github.com>
2019-10-06 02:40:56 +0900

    Add files via upload

---
+++ QuickSort.c
@@ -0,0 +1,52 @@
+/* QuickSort.c */
+
+// pivot is end of array
+int Partition1(int arr[], int left, int right) {
+	int target = arr[right];
+	int low = left, high = left;
+	int temp;
+
+	for (; high < right; high++) {
+		if (arr[high] < target) {
+			temp = arr[high];
+			arr[high] = arr[low];
+			arr[low++] = temp;
+		}
+	}
+
+	arr[right] = arr[low];
+	arr[low] = target;
+
+	return low;
+}
+
+// pivot is start of array
+int Partition2(int arr[], int left, int right) {
+	int target = arr[left];
+	int low = left + 1, high = left + 1;
+	int temp;
+
+	for (; high <= right; high++) {
+		if (arr[high] < target) {
+			temp = arr[high];
+			arr[high] = arr[low];
+			arr[low++] = temp;
+		}
+	}
+
+	arr[left] = arr[low - 1];
+	arr[low - 1] = target;
+
+	return low - 1;
+}
+
+void QuickSort(int arr[], int left, int right) {
+	if (left >= right) return;
+	else {
+		int mid = 
+			// Partition1(arr, left, right);
+			// Partition2(arr, left, right);
+		QuickSort(arr, left, mid - 1);
+		QuickSort(arr, mid + 1, right);
+	}
+}

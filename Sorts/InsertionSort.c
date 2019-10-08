d04afcc996680560b9b8805e9a509fef8d8c314b
Jin-Book <taejin7824@gmail.com>
2019-10-08 22:14:51 +0900

    InsertionSort but need fix error

---
+++ Sorts/InsertionSort.c
@@ -0,0 +1,10 @@
+/* InsertiontionSort.c */
+void InsertionSort(int arr[], int len) {
+	for (int i = 1; i < len; i++) {
+		int cmp;
+		int target = arr[i];
+		for (cmp = i - 1; target < arr[cmp]; cmp--)
+			arr[cmp + 1] = arr[cmp];
+		arr[cmp + 1] = target;
+	}
+}

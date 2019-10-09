a1c7bc10983ad3cff767bc5c0abdf90854e21066
Jin-Book <taejin7824@gmail.com>
2019-10-09 17:32:13 +0900

    Complete Insertion.c

--- Sorts/InsertionSort.c
+++ Sorts/InsertionSort.c
@@ -3,7 +3,7 @@ void InsertionSort(int arr[], int len) {
 	for (int i = 1; i < len; i++) {
 		int cmp;
 		int target = arr[i];
-		for (cmp = i - 1; target < arr[cmp]; cmp--)
+		for (cmp = i - 1; (target < arr[cmp]) && (cmp >= 0); cmp--)
 			arr[cmp + 1] = arr[cmp];
 		arr[cmp + 1] = target;
 	}

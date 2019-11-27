fd5814ace7fae00dfd27ceb1ea4058663324a752
Jin-Mac <taejin7824@gmail.com>
2019-11-27 21:13:20 +0900

    Add Queue List for BFS

--- DataStructure/C/Graph/Graph.c
+++ DataStructure/C/Graph/Graph.c
@@ -1,6 +1,7 @@
 // Graph.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct _ListNode {
 	int val;
@@ -12,6 +13,41 @@ typedef struct _AdjacentList {
 	ListNodePtr end[20];
 	int vertexSize;
 } AdjacentList, * AdjacentListPtr;
+
+typedef struct _QueueList {
+	ListNodePtr front;
+	ListNodePtr rare;
+} QueueList, * QueueListPtr;
+
+
+void InitQueue(QueueListPtr queue) {
+	queue->front = queue->rare = NULL;
+}
+
+bool isEmpty(QueueList queue) {
+	return queue.front == NULL;
+}
+
+void Enqueue(QueueListPtr queue, int val) {
+	ListNodePtr newNode = malloc(sizeof(ListNode));
+	newNode->val = val;
+	newNode->next = NULL;
+
+	if (isEmpty(queue))
+		queue->front = queue->rare = newNode;
+	else {
+		queue->rare->next = newNode;
+		queue->rare = newNode;
+	}
+}
+
+int Dequeue(QueueListPtr queue) {
+	ListNodePtr remove = queue->front;
+
+	queue->front = queue->front->next;
+
+	free(remove);
+}
 
 void InitAdjList(AdjacentListPtr adjList, int size) {
 	adjList->vertexSize = size;
@@ -45,8 +81,20 @@ void PrintGraph(AdjacentList adjList) {
 	}
 }
 
-void DFS(AdjacentList adjList);
-void BFS(AdjacentList adjList);
+void DFS(AdjacentList adjList) {
+
+}
+
+void BFS(AdjacentList adjList) {
+	QueueList queue;
+	InitQueue(&queue);
+
+	Enqueue(&queue, adjlist.vertex[0].val)
+	while (! isEmpty()) {
+		adjlist.vertex[Dequeue(&queue)].val
+	}
+
+}
 
 void main(void) {
 	AdjacentList adjList;

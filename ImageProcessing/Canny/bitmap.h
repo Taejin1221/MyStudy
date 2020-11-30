1a7947cb436570cfe9531c04802f0dace8d67f06
Jin-MacBook <taejin7824@gmail.com>
2020-12-01 02:24:45 +0900

    New Canny Edge Project

---
+++ ImageProcessing/Canny/bitmap.h
@@ -0,0 +1,39 @@
+// bitmap.h
+#ifndef BITMAP
+#define BITMAP BITMAP
+
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+typedef int INT;
+
+typedef struct tagBITMAPFILEHEADER {
+	WORD bfType;
+	DWORD bfSize;
+	WORD bfReserved1;
+	WORD bfReserved2;
+	DWORD bfOffBits;
+} BITMAPFILEHEADER, *PBITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER {
+	DWORD biSize;
+	INT biWidth;
+	INT biHeight;
+	WORD biPlanes;
+	WORD biBitCount;
+	DWORD biCompression;
+	DWORD biSizeImage;
+	INT biXPelsPerMeter;
+	INT biYPelsPerMeter;
+	DWORD biClrUsed;
+	DWORD biClrImportant;
+} BITMAPINFOHEADER, *PBITMAPINFOHEADER;
+
+typedef struct tagRGBQUAD {
+	BYTE rgbBlue;
+	BYTE rgbGreen;
+	BYTE rgbRed;
+	BYTE rgbReserved;
+} RGBQUAD;
+
+#endif
---
+++ ImageProcessing/Canny/bmp2bmp.c
@@ -0,0 +1,48 @@
+// bmp2bmp.c
+#include <stdio.h>
+#include <stdlib.h>
+#include "bitmap.h"
+
+#define WIDTHBYTES(b) ( ( ( b ) + 3 ) / 4 * 4 )
+
+int main(void) {
+	BITMAPFILEHEADER fileHeader;
+	BITMAPINFOHEADER infoHeader;
+	RGBQUAD rgbQuad[256];
+
+	FILE* fp = fopen( "Lenna.bmp", "rb" );
+
+	fread( &fileHeader, sizeof(BITMAPFILEHEADER), 1, fp );
+	fread( &infoHeader, sizeof(BITMAPINFOHEADER), 1, fp );
+	fread( rgbQuad, sizeof(RGBQUAD), 256, fp );
+
+	INT width = infoHeader.biWidth, height = infoHeader.biHeight;
+	INT stepWidth = WIDTHBYTES( ( infoHeader.biBitCount / 8 ) * width );
+
+	fseek( fp, fileHeader.bfOffBits, SEEK_SET );
+	BYTE** inputImg = (BYTE**)malloc(sizeof(BYTE*) * height);
+	for ( int i = 0; i < height; i++ ) {
+		inputImg[i] = (BYTE*)malloc(sizeof(BYTE) * stepWidth);
+		fread( inputImg[i], sizeof(BYTE), stepWidth, fp );
+	}
+
+	fclose(fp);
+
+	fp = fopen( "Lenna(bmp_bmp).bmp", "wb" );
+
+	fwrite( &fileHeader, sizeof(BITMAPFILEHEADER), 1, fp );
+	fwrite( &infoHeader, sizeof(BITMAPINFOHEADER), 1, fp );
+	fwrite( rgbQuad, sizeof(RGBQUAD), 256, fp );
+	fseek( fp, fileHeader.bfOffBits, SEEK_SET );
+
+	for ( int i = 0; i < height; i++ )
+		fwrite( inputImg[i], sizeof(BYTE), stepWidth, fp );
+
+	fclose(fp);
+
+	for ( int i = 0; i < height; i++ )
+		free( inputImg[i] );
+	free( inputImg );
+
+	return 0;
+}

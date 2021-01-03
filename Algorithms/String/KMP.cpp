d52d02a2c145859bd9d20acd49c28621102cd008
JIN-DESK <taejin7824@gmail.com>
2021-01-03 20:48:23 +0900

    KMP Algorithm with C++

---
+++ Algorithms/String/KMP.cpp
@@ -0,0 +1,35 @@
+/*
+ * File Name: KMP.cpp
+ * Author: Taejin, An
+ * Date: 01/03/2021
+ * Description: C++ code of Knuth-Morris-Pratt Algorithm
+*/
+
+vector<int> KMP( string text, string pattern ) {
+	int textSize = text.size(), patternSize = pattern.size();
+
+	vector<int> fail ( patternSize );
+	for ( int i = 0, j = 1; i < patternSize; i++ ) {
+		while ( j > 0 && pattern[i] != pattern[j] )
+			j = fail[j - 1];
+
+		if ( pattern[i] == pattern[j] )
+			fail[i] = ++j;
+	}
+
+	vector<int> indexes;
+	for ( int i = 0; j = 0; i < textSize; i++ ) {
+		while ( j > 0 && text[i] != pattern[j] )
+			j = fail[j - 1];
+
+		if ( text[i] == pattern[j] ) {
+			j++;
+			if ( j == patternSize )
+				indexes.push_back( i - patternSize + 1 );
+			else
+				j = fail[j - 1];
+		}
+	}
+
+	return indexes;
+}
